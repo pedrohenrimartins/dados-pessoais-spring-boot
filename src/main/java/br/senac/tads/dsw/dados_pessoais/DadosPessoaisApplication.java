63a82734e17732613dfc3a08d7ac2f8a94e64f70
pedrohenrimartins <pedrinhogmartins2016@gmail.com>
2026-09-07 12:59:08 -0300

    Initial commit

---
+++ src/main/java/br/senac/tads/dsw/dados_pessoais/DadosPessoaisApplication.java
@@ -0,0 +1,14 @@
+// hash-identificacao: 92158ab623ed97e1843e9f6c9ff544c3225866608cbbccab2d7190b9fa7602f7
+package br.senac.tads.dsw.dados_pessoais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadosPessoaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DadosPessoaisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/senac/tads/dsw/dados_pessoais/DadosPessoaisApplicationTests.java
@@ -0,0 +1,14 @@
+// hash-identificacao: 92158ab623ed97e1843e9f6c9ff544c3225866608cbbccab2d7190b9fa7602f7
+package br.senac.tads.dsw.dados_pessoais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DadosPessoaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
